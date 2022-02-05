f14aec14daa4d3dbac0b75dfee0b7ef9683938ea
IBassemTarek <bassemtarekmahrous@gmail.com>
2022-02-05 04:36:43 +0200

    cc

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,15 +1,13 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { User } from "./src/user/entities/user.entities";
 
 export const typeOrmConfig:TypeOrmModuleOptions = {
-    "type": "postgres",
-    "host": "localhost",
-    "port": 5432,
-    "username": "root",
-    "password": "root",
-    "database": "test_db",
-    "synchronize": true,
-    "entities": [ 'dist/**/*.entity.js'],
-    "migrations": [
-       "dist/src/db/migrations/*.ts"
-    ]
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "root",
+    password: "root",
+    database: "test_db",
+    synchronize: true,
+    entities: [User],
 }; 
